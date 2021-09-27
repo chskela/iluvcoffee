e1c708b6bd40bd9c7e20480633c144c36d478377
Aleksey Chistyakov <chskela@gmail.com>
2021-09-27 20:42:41 +0300

    add coffee-rating, coffees.constants

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true,
       synchronize: true,
     }),
+    CoffeeRatingModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -4,12 +4,25 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { CoffeesService } from './coffees.service';
+import { COFFEE_BRANDS } from './coffees.constants';
 import { Event } from 'src/events/entities/event.entity';
 import { CoffeesController } from './coffees.controller';
+import { Connection } from 'typeorm';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  providers: [
+    CoffeesService,
+    {
+      provide: COFFEE_BRANDS,
+      useFactory: async (connection: Connection): Promise<string[]> => {
+        const coffeeBrands = await Promise.resolve(['buddy brew', 'nescafe']);
+        return coffeeBrands;
+      },
+      inject: [Connection],
+    },
+  ],
+  exports: [CoffeesService],
 })
 export class CoffeesModule {}
